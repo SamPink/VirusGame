e971a401c5829704335c7e014bac8d007bb3b0e2
sampink <samuel.pink@outlook.com>
2020-04-24 13:46:35 +0100

    Added new start activity

---
+++ app/src/main/java/com/nh006220/VirusGame/StartActivity.java
@@ -0,0 +1,14 @@
+package com.nh006220.VirusGame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
